2276decbf9f38515d56a3f4a58eb5984a3645c4a
simran-1296 <simran-1296@users.noreply.github.com>
2026-02-28 23:02:20 +0000

    Add models, migrations, serializers, admin registration

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Team, TeamMember, Monitor, MonitorResult, Incident, NotificationLog, Invite
 
-# Register your models here.
+admin.site.register(Team)
+admin.site.register(TeamMember)
+admin.site.register(Monitor)
+admin.site.register(MonitorResult)
+admin.site.register(Incident)
+admin.site.register(NotificationLog)
+admin.site.register(Invite)
