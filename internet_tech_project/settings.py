9b524d9e70355252be4d345c8364a99ef97914c7
apple <apple@inuMacBook-Pro.local>
2026-03-01 14:33:54 +0800

    feat: configure database connection to upty_db

--- internet_tech_project/settings.py
+++ internet_tech_project/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'internet_tech_project.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": "upty_db",
+        "USER": "postgres",
+        "PASSWORD": "PiaNo4te88$",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
