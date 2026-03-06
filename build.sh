1d41eb7686947cc62c5d24044c6ba3a1a5e44923
apple <apple@inuMacBook-Pro.local>
2026-03-06 12:25:48 +0800

    Add build script to fix Pillow installation

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+apt-get update && apt-get install -y libjpeg-dev zlib1g-dev
+pip install --upgrade pip
+pip install -r requirements.txt
+python manage.py collectstatic --no-input --clear
