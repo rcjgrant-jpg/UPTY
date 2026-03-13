b5b3919df6bbdf82289c88374d0997c3b7f96da0
rcjgrant-jpg <rcjgrant@gmail.com>
2026-03-13 22:50:20 +0000

    Config to set monitor scheduler upon app start

--- api/apps.py
+++ api/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+import os
 
 
 class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        if os.environ.get("RUN_MAIN") == "true":
+            from .scheduler import start_monitor_scheduler
+            start_monitor_scheduler()
