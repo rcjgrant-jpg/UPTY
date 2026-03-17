34efe54176e7538f598062da58fb06dc5b4a3880
simran-1296 <simran-1296@users.noreply.github.com>
2026-03-17 00:56:06 +0000

    Add migrate to build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ apt-get update && apt-get install -y libjpeg-dev zlib1g-dev
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
+python manage.py migrate
