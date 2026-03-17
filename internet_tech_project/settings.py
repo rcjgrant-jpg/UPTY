75e361660dade7d0bfeb9d24f6c01a6101dfa15e
simran-1296 <simran-1296@users.noreply.github.com>
2026-03-17 13:16:24 +0000

    Add localhost ports to CSRF trusted origins

--- internet_tech_project/settings.py
+++ internet_tech_project/settings.py
@@ -45,8 +45,21 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:4173",
     "http://127.0.0.1:4173",
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'https://upty.onrender.com',
 
-    # add deployed frontend URL here later
+    # add deployed frontend URL here later if applicable
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://localhost:4173',
+    'http://127.0.0.1:4173',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'https://upty.onrender.com',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
